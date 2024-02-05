099fb84300a177e0222fc6c2cfcd26032ea91be9
Anton Ziuzikov <zuzikov.anton@gmail.com>
2024-02-05 16:47:50 +0300

    feat: implement some side effects (fetching geolocation and saving data into local storage)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import { useRef, useState } from 'react';
+import {useEffect, useRef, useState} from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import {sortPlacesByDistance} from "./loc.js";
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [availablePlaces, setAvailablePlaces] = useState([])
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(AVAILABLE_PLACES,
+          position.coords.latitude,
+          position.coords.longitude)
+      setAvailablePlaces(sortedPlaces);
+    });
+  }, []);//empty dependencies never change, so this useEffect function will not execute again.
+
+
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -28,6 +41,11 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces'));
+    if (storedIds.indexOf(id) === -1 ) {
+      localStorage.setItem('selectedPlaces', JSON.stringify([id, ...storedIds]));
+    }
   }
 
   function handleRemovePlace() {
@@ -63,7 +81,8 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
+          fallbackText="Sorting places by distance..."
           onSelectPlace={handleSelectPlace}
         />
       </main>
